0c06134d922a37b14cbb6c5c957cd306ec6cf5a9
OlinadWiz <testxesperimenti@gmail.com>
2025-06-06 07:08:48 +0200

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Frontend React per Emulatore Enigma2</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
